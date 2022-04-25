0f421219919b48467371ad8c5405035241f6deaa
Regan Smith <pregansmith@gmail.com>
2022-04-24 22:05:19 -0400

    Added dark mode to home page after 7 pm (still a work in progress)

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+// Dark mode for home page
+if (new Date().getHours() > 18) {
+  const html = document.querySelector('footer');
+  html.style.backgroundColor = '#000000';
+  html.style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const html = document.querySelector('header');
+  html.style.backgroundColor = '#000000';
+  html.style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const html = document.querySelector('html');
+  html.style.backgroundColor = '#000000';
+  html.style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const html = document.querySelector('features');
+  html.style.backgroundColor = '#000000';
+  html.style.color = 'white';
+}
